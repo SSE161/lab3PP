dcd7c19ad57aae53b1318ce8377096aa0f1056dd
SSE161 <alanovg@yandex.ru>
2022-12-14 17:06:17 +0300

    commit with ip checker

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import re
 
+regex = "([0-9]{1,3}[\.]){3}[0-9]{1,3}"
+
 
 def ip_checker(string):
-    return True
+    o = re.match(regex, string)
+    if o is None:
+        return False
+    else:
+        return True
 
 
+print(ip_checker("192.168.0.1"))
